18835bf25fc9783fec36db5194ae523d38d932a8
GITA <gitanjalee@marmeto.com>
2025-02-27 22:58:59 +0530

    this code for slider image

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// this method for capture all the element 
 const body = document.body
 const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
-let activeSlide = 0
 
+let activeSlide = 0
+// this method for right side btn when you click it .it will be increment 
 rightBtn.addEventListener('click', () => {
   activeSlide++
 
@@ -16,6 +18,7 @@ rightBtn.addEventListener('click', () => {
   setActiveSlide()
 })
 
+// this method for left btn.when you click it .it will be decrease a number
 leftBtn.addEventListener('click', () => {
   activeSlide--
 
@@ -28,11 +31,12 @@ leftBtn.addEventListener('click', () => {
 })
 
 setBgToBody()
-
+// this method for all the background image add
 function setBgToBody() {
   body.style.backgroundImage = slides[activeSlide].style.backgroundImage
 }
 
+// 
 function setActiveSlide() {
   slides.forEach((slide) => slide.classList.remove('active'))
 
